36104f1885aa513739f20ec0bdda6fb715b4b334
Muhammad Hasbi <onlyhasbi@gmail.com>
2023-10-19 00:33:04 +0800

    get login input value

--- src/element/SignInForm.tsx
+++ src/element/SignInForm.tsx
@@ -6,22 +6,44 @@ import {
   Checkbox,
   Button,
 } from '@mui/material';
-import React from 'react';
+import React, { ChangeEvent, FormEventHandler } from 'react';
 
+type AuthPayload = {
+  username?: string;
+  password?: string;
+};
 function SignInForm() {
+  const [payload, setPayload] = React.useState<AuthPayload | null>(null);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPayload((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    window.alert(JSON.stringify(payload));
+  };
+
   return (
     <Card sx={{ minWidth: 260, maxWidth: 560, width: 560 }}>
-      <form style={{ padding: 30 }}>
+      <form style={{ padding: 30 }} onSubmit={handleSubmit}>
         <Stack direction="column" spacing={2}>
           <TextField
             sx={{ width: '100%' }}
             label="Your Email / Username"
+            name="username"
+            onChange={handleChange}
+            value={payload?.username || ''}
             variant="outlined"
             size="small"
           />
           <TextField
             sx={{ width: '100%' }}
             label="Enter Password"
+            type="password"
+            name="password"
+            onChange={handleChange}
+            value={payload?.password || ''}
             variant="outlined"
             size="small"
           />
@@ -41,7 +63,11 @@ function SignInForm() {
               Forgot Password
             </Button>
           </Stack>
-          <Button variant="contained" sx={{ textTransform: 'none' }}>
+          <Button
+            variant="contained"
+            sx={{ textTransform: 'none' }}
+            type="submit"
+          >
             Login
           </Button>
         </Stack>
